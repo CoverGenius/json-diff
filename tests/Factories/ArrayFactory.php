030dd0e8713a528c3cf20884d0041c0e34c73d8b
Ruben Funai <ruben.funai@gmail.com>
2023-05-04 08:47:28 +1000

    feat: implement array factory

---
+++ tests/Factories/ArrayFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jet\Tests\Factories;
+
+use Illuminate\Support\Arr;
+
+class ArrayFactory
+{
+    public static function new(): ArrayFactory
+    {
+        return new self();
+    }
+
+    public function create(array $extra = []): array
+    {
+        return $extra + [
+            'name' => fake()->name,
+            'age' => fake()->numberBetween(0, 100),
+            /** @see https://en.wikipedia.org/wiki/List_of_tallest_people */
+            'height' => fake()->randomFloat(2, 0, 272),
+            'sports' => SportsArrayFactory::new()->create(),
+            'is_active' => Arr::random([false, true]),
+            'abn' => Arr::random([
+                null,
+                (string) fake()->numberBetween()
+            ])
+        ];
+    }
+}
