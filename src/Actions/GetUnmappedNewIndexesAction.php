e75be4b3a6400ebb9ae69f1a16cf29a1717c3e2e
Ruben Funai <ruben.funai@gmail.com>
2023-05-04 09:56:03 +1000

    feat: implement get unmapped new indexes action

---
+++ src/Actions/GetUnmappedNewIndexesAction.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jet\JsonDiff\Actions;
+
+use Illuminate\Support\Collection;
+use Jet\JsonDiff\DiffMapping;
+
+class GetUnmappedNewIndexesAction
+{
+    /**
+     * @param Collection<DiffMapping> $diffMappings
+     * @param array $new
+     * @return Collection<int>
+     */
+    public function execute(Collection $diffMappings, array $new): Collection
+    {
+        return collect(array_keys($new))
+            ->diff(
+                $diffMappings
+                    ->map(function (?DiffMapping $diffMapping) {
+                        return optional($diffMapping)->getNewIndex();
+                    })
+                    ->filter(function (?int $index) {
+                        return $index !== null;
+                    })
+            )
+            ->values();
+    }
+}
