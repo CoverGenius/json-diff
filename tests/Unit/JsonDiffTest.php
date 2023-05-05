4363bc0c00962697a60ad89e29e116fa9d8bbddf
Ruben Funai <ruben.funai@gmail.com>
2023-05-05 16:49:07 +1000

    feat: implement get number of changes unit test

---
+++ tests/Unit/JsonDiffTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jet\Tests\Unit;
+
+use Jet\JsonDiff\JsonDiff;
+use PHPUnit\Framework\TestCase;
+
+class JsonDiffTest extends TestCase
+{
+    public function test_get_number_of_changes(): void
+    {
+        $jsonDiff = new JsonDiff([], []);
+        $this->assertSame(
+            0,
+            $jsonDiff->getNumberOfChanges()
+        );
+
+        $jsonDiff = new JsonDiff(
+            ["apple"],
+            ["orange"]
+        );
+        $this->assertSame(
+            1,
+            $jsonDiff->getNumberOfChanges()
+        );
+
+        $jsonDiff->addAddedKey('', '0', 'a string');
+        $this->assertSame(
+            2,
+            $jsonDiff->getNumberOfChanges()
+        );
+
+        $jsonDiff->addRemovedKey('', '0', 'a string');
+        $this->assertSame(
+            3,
+            $jsonDiff->getNumberOfChanges()
+        );
+    }
+}
