b748b921f91f4e068a01d5047037caaabea2e3b4
Ruben Funai <ruben.funai@gmail.com>
2023-05-05 14:14:42 +1000

    feat: implement test for path actions

---
+++ tests/Unit/Actions/GetItemPathActionTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jet\Tests\Unit\Actions;
+
+use Jet\JsonDiff\Actions\GetItemPathAction;
+use PHPUnit\Framework\TestCase;
+
+class GetItemPathActionTest extends TestCase
+{
+    public function test_it_returns_the_item_path(): void
+    {
+        $action = new GetItemPathAction();
+
+        $this->assertSame(
+            '0',
+            $action->execute('', 0)
+        );
+        $this->assertSame(
+            '0.name',
+            $action->execute('0.', 'name')
+        );
+    }
+}
